d7409d353b3bcde5cabf63219a9ab3e6b5f0e5b7
cristianekoshino <cristianekoshino@gmail.com>
2020-05-25 14:39:42 +0100

    second section created

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,28 @@ class App extends Component {
           <p>You will learn a Frontend framework from scratch, to became a Ninja developer.</p>
           <button>Awesome!</button>
         </div>
+        <div className="App-second-section">
+          <div className="items-second-section">
+            <img src="/images/icon1.png" alt=""/> 
+            <h2>Declarative</h2>
+            <p>React makes it painless to create interative UIs.</p> 
+          </div>
+          <div className="items-second-section">
+            <img src="/images/icon2.png" alt=""/>  
+            <h2>Components</h2>
+            <p>Build encapsulated components that manage their state.</p>
+          </div>
+          <div className="items-second-section">
+            <img src="/images/icon3.png" alt=""/>  
+            <h2>Single Way</h2>
+            <p>A set of immutable values are passed to the components.</p>
+          </div>
+          <div className="items-second-section">
+            <img src="/images/icon4.png" alt=""/>  
+            <h2>JSX</h2>
+            <p>Statically-typed, designed to run on moderns browsers.</p>
+          </div>
+        </div> 
       </div>
     );
   }
